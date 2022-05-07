ca9106bcda7194a1738797192ba6a78a872bc68e
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-05-07 21:30:30 +0300

    hw-10-js+import

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from "./js/fetchCountries";
+import { debounce, throttle } from 'lodash';
+
 
 const DEBOUNCE_DELAY = 300;
