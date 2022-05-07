d624d1bad7ecbe013bcb1ab43a9d1e4f41239e73
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-05-07 23:17:39 +0300

    hw-10-js+npx browserslist@latest --update-db

--- src/index.js
+++ src/index.js
@@ -3,5 +3,31 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { fetchCountries } from "./js/fetchCountries";
 import { debounce, throttle } from 'lodash';
 
-
 const DEBOUNCE_DELAY = 300;
+const searchBox = document.querySelector('#search-box');
+const countryEl = document.querySelector('.country-list');
+const infoEl = document.querySelector('.country-info');
+
+searchBox.addEventListener('input', debounce(e => {
+  e.preventDefault();
+}, DEBOUNCE_DELAY));
+
+function showCountries(countries) {
+  countryEl.innerHTML = '';
+  infoEl.innerHTML = '';
+  if (countries.length > 10) {
+    return Notify.info('Too many matches found. Please enter a more specific name.');
+  };
+  if (countries.length >= 2) {
+    markupCountries(countries);
+  };
+  markupCountry(countries);
+};
+
+function markupCountries(countries) {
+
+};
+
+function markupCountry(countries) {
+
+};
